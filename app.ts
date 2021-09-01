9d4a279eb2a9a253761175f698d9822aec96fcfe
naijun0403 <bluerex@mcna.xyz>
2021-09-01 23:29:38 +0900

    일단 모듈 업로드 됨

--- app.ts
+++ app.ts
@@ -4,11 +4,10 @@
  * This code is licensed under the MIT Licensing Principles.
  */
 
-import express from "express";
+import express, { NextFunction } from "express";
 import path from "path";
 import logger from "morgan";
 import cookieParser from "cookie-parser";
-import subdomain from "express-subdomain";
 import fileupload from 'express-fileupload'
 
 import indexRouter from "./routes/index";
@@ -26,9 +25,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(subdomain('kpm', indexRouter))
 app.use(fileupload({
-    
+    limits: { fileSize: 50 * 1024 * 1024 },
 }))
 
 app.use('/', indexRouter);
@@ -40,7 +38,7 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use((err: any, req: express.Request, res: express.Response, next: NextFunction) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.ts
+++ routes/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import { UploadedFile } from "express-fileupload";
+import fs from 'fs'
 const router = express.Router();
+
+interface saveModuleInterface {
+    id: number;
+    name: string;
+}
 
 router.get('/', (req, res, next) => {
     res.render('index', { title: 'Express' });
 })
 
-router.post('/upload', (req, res, next) => {
+// receive file from /kpm/upload and save file to /kpm/uploads
+router.post('/upload/kpm', (req, res, next) => {
     if(!req.files || Object.keys(req.files).length === 0) {
         res.status(400).json({
             status: 400,
@@ -15,10 +22,29 @@ router.post('/upload', (req, res, next) => {
         return;
     }
 
-    let modulesFile:any = req.files.module;
-    let uploadPath = __dirname + '/uploads/' + modulesFile.name;
+    let modulesFile = req.files.module as UploadedFile;
+    const savedFile = JSON.parse(fs.readFileSync('/home/ubuntu/express-api/modules.json', 'utf8'));
 
-    modulesFile.mv(uploadPath, (err) => {
+    let isSuccess = true
+
+    savedFile.map((e: saveModuleInterface) => {
+        if(e.name === modulesFile.name) {
+            isSuccess = false;
+            return;
+        }
+    });
+
+    if(!isSuccess) {
+        return res.status(400).json({
+            status: -484,
+            message: 'already exists file name'
+        });
+    }
+
+    let bfi = savedFile[0].id
+    let uploadPath = `/home/ubuntu/express-api/uploads/${bfi}.zip`
+
+    modulesFile.mv(uploadPath, (err: any) => {
         if(err) {
             return res.status(500).json({
                 status: 500,
@@ -26,11 +52,13 @@ router.post('/upload', (req, res, next) => {
             });
         }
 
+        savedFile.unshift({ id: ++bfi, name: modulesFile.name })
+        fs.writeFileSync('/home/ubuntu/express-api/modules.json', JSON.stringify(savedFile, null, 4))
         res.status(200).json({
             status: 0,
-            id: 1,
+            id: savedFile[0].id,
             message: 'Success upload module'
-        })
+        });
     })
 })
 
